3de50316663b5dfa6aace656a0abb042b6131211
Wesley Tjangnaka <wesley.tjangnaka@gmail.com>
2023-03-05 16:24:26 -0800

    Python option

---
+++ data_processing.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+# Read the CSV files with Pandas
+game1 = pd.read_csv('./csvs/game_1_data.csv')
+game2 = pd.read_csv('./csvs/game_2_data.csv')
+game3 = pd.read_csv('./csvs/game_3_data.csv')
+game4 = pd.read_csv('./csvs/game_4_data.csv')
+game5 = pd.read_csv('./csvs/game_5_data.csv')
+game6 = pd.read_csv('./csvs/game_6_data.csv')
+game7 = pd.read_csv('./csvs/game_7_data.csv')
+game8 = pd.read_csv('./csvs/game_8_data.csv')
+game9 = pd.read_csv('./csvs/game_9_data.csv')
+game10 = pd.read_csv('./csvs/game_10_data.csv')
+prev_season = pd.read_csv('./csvs/previous_season_results.csv')
+season_matchups = pd.read_csv('./csvs/season_match_up.csv')
+
+
+
+
+
